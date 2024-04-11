2d3c3e3852547ce78982da5cae9f68c6b79233be
dyl <anduytran@ufl.edu>
2024-04-11 04:40:01 -0400

    Finished the HashMap and added a main that tests pulling values from the sheet and can find the 10 lowest calorie values

--- src/hashmap.h
+++ src/hashmap.h
@@ -10,9 +10,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool sortbysecond(const pair<string, float> &a, const pair<string, float> &b)
+{
+    return (a.second < b.second);
+}
+
 class HashNode{
     string key;
-    HashNode* next = nullptr;
     pair<string, vector<float>> value;
 public:
     HashNode(string k = "", pair<string, vector<float>> v = {"NULL", {}}){
@@ -22,22 +26,29 @@ public:
     string getKey(){
         return key;
     }
-    void setValue(pair<string, vector<float>> value){
-        this->value = value;
-    }
     pair<string, vector<float>> getValue(){
         return value;
     }
 
+    HashNode* operator=(HashNode* ptr){
+        this->key = ptr->key;
+        this->value = ptr->value;
+        return this;
+    }
 };
 
 class HashMap{
     vector<HashNode*> map;
+    int current_size = 0;
+    int max_size;
+    float lf = .7;
 
 public:
+    hash<string> hash;
     HashMap(){
-        vector<HashNode*> map(4294967295);
-        this->map = map;
+        max_size = 100;
+        vector<HashNode*> v(max_size);
+        this->map = v;
     }
     ~HashMap(){
         for(int i = 0; i < map.size(); i++){
@@ -46,25 +57,75 @@ public:
             map[i] = nullptr;
         }
     }
-    void set(string name, int key, pair<string, vector<float>> value){
-        HashNode* position = map[key];
-        if(position == nullptr){
-            position = new HashNode(name, value);
+    int getSize(){
+        return max_size;
+    }
+    int getCurrentSize(){
+        return current_size;
+    }
+    HashNode*& operator[](string name){
+        current_size += 1;
+        int index = hash(name) % (max_size - 1);
+        if(!map[index]){
+            return map[index];
         }
-        else{
-            if(map[key]->getKey() != name){
-                //probing
+        if(map[index]->getKey() != name){
+            while(map[index] != nullptr){
+                int power = 0;
+                index += pow(2, power);
+                power++;
+                if(index == getSize()){
+                    index = 0;
+                }
             }
-            position->setValue(value);
+        }
+        if((float)current_size / (float)max_size >= lf){
+            max_size *= 2;
+            if(max_size == 12800){
+                max_size = 20300;
+            }
+            index = hash(name) % (max_size - 1);
+            vector<HashNode*> temp(max_size);
+            for(int i = 0; i < map.size(); i++){
+                if(map[i] != nullptr){
+                    temp[index] = map[i];
+                }
+            }
+            map = temp;
+        }
+        return map[index];
+    }
+    HashNode* operator[](string name) const{
+        return map[hash(name) % (max_size - 1)];
+    }
+    void tenLowestCalorie(const string& category) {
+        vector<pair<string, float>> temp;
+        for (int i = 0; i < getSize(); i++) {
+            if (map[i] != nullptr) {
+                if (map[i]->getValue().first == category) {
+                    float norm_cal = map[i]->getValue().second[1] * (map[i]->getValue().second[14] / 100);
+                    if(norm_cal < 0){
+                        continue;
+                    }
+                    if (temp.size() < 10) {
+                        temp.emplace_back(map[i]->getKey(), norm_cal);
+                        if (temp.size() == 10) {
+                            sort(temp.begin(), temp.end(), sortbysecond);
+                        }
+                    } else {
+                        if (norm_cal < temp.back().second && norm_cal > 0) {
+                            sort(temp.begin(), temp.end(), sortbysecond);
+                            temp.pop_back();
+                            temp.emplace_back(map[i]->getKey(), norm_cal);
+                        }
+                    }
+                }
+            }
+        }
+        sort(temp.begin(), temp.end(), sortbysecond);
+        cout << "The 10 foods with the lowest calorie values are: " << endl;
+        for (int i = 0; i < temp.size(); i++) {
+            cout << i + 1 << ". " << temp[i].first << ": " << temp[i].second << endl;
         }
     }
-    pair<string, vector<float>> operator[](string name){
-        hash<string> hash;
-        return map[hash(name)]->getValue();
-    }
-    const pair<string, vector<float>>& operator[](string name) const{
-        hash<string> hash;
-        return map[hash(name)]->getValue();
-    }
-
 };
---
+++ src/main.cpp
@@ -0,0 +1,101 @@
+//
+// Created by Dylan on 4/9/2024.
+//
+#include <iostream>
+#include "hashmap.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void load(HashMap &hashmap){
+    fstream file;
+    file.open("MyFoodData Nutrition Facts SpreadSheet Release 1.4 - SR Legacy and FNDDS.csv", ios::in | ios::app);
+    string temp, line, word, code, description, nutrient_code;
+    getline(file, temp);
+    getline(file, temp);
+    getline(file, temp);
+    getline(file, temp);
+    while(getline(file, line)){
+        stringstream s(line);
+        getline(s, word, ',');
+        //ptr->ID = stof(word);
+        getline(s, word, ',');
+        string name = word;
+        getline(s, word, ',');
+        string food_group = word;
+        getline(s, word, ',');
+        float calories = stof(word);
+        getline(s, word, ',');
+        float protein = stof(word);
+        getline(s, word, ',');
+        float carbohydrates = stof(word);
+        getline(s, word, ',');
+        float sugars = stof(word);
+        getline(s, word, ',');
+        float fiber = stof(word);
+        getline(s, word, ',');
+        float cholesterol = stof(word);
+        getline(s, word, ',');
+        float saturated_fats = stof(word);
+        getline(s, word, ',');
+        float trans_fats = stof(word);
+        getline(s, word, ',');
+        float soluble_fiber = stof(word);
+        getline(s, word, ',');
+        float insoluble_fiber = stof(word);
+        getline(s, word, ',');
+        float monounsaturated_fats = stof(word);
+        getline(s, word, ',');
+        float polyunsaturated_fats = stof(word);
+        getline(s, word, ',');
+        float caffeine = stof(word);
+        getline(s, word, ',');
+        float sw = stof(word);
+        getline(s, word, ',');
+        string sd = word;
+        getline(s, word, ',');
+        float sw2 = stof(word);
+        getline(s, word, ',');
+        string sd2 = word;
+        getline(s, word, ',');
+        float sw3 = stof(word);
+        getline(s, word, ',');
+        string sd3 = word;
+        getline(s, word, ',');
+        float sw4 = stof(word);
+        getline(s, word, ',');
+        string sd4 = word;
+        getline(s, word, ',');
+        float sw5 = stof(word);
+        getline(s, word, ',');
+        string sd5 = word;
+        getline(s, word, ',');
+        float sw6 = stof(word);
+        getline(s, word, ',');
+        string sd6 = word;
+        getline(s, word, ',');
+        float sw7 = stof(word);
+        getline(s, word, ',');
+        string sd7 = word;
+        getline(s, word, ',');
+        float sw8 = stof(word);
+        getline(s, word, ',');
+        string sd8 = word;
+        getline(s, word, ',');
+        float sw9 = stof(word);
+        getline(s, word, ',');
+        string sd9 = word;
+        getline(s, word, ',');
+        float twohundredcal_weight = stof(word);
+        vector<float> v = {calories, protein, carbohydrates, sugars, fiber, cholesterol, saturated_fats, trans_fats, soluble_fiber, insoluble_fiber, monounsaturated_fats, polyunsaturated_fats, caffeine, sw, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9, twohundredcal_weight};
+        HashNode* ptr = new HashNode(name, make_pair(food_group, v));
+        hashmap[name] = ptr;
+    }
+}
+
+int main() {
+    HashMap map;
+    load(map);
+    map.tenLowestCalorie("Meats");
+    return 0;
+}
