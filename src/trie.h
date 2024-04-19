7e6cf296b4bef76cb2bb2e72e62a5bb8caa3db4f
jepin <pinzon.jenrique@ufl.edu>
2024-04-18 20:30:48 -0500

    Some updates in trie header file - vocabulary size and priority queue

--- src/trie.h
+++ src/trie.h
@@ -5,10 +5,11 @@
 #ifndef TESTING_TRIE2_H
 #define TESTING_TRIE2_H
 #include <iostream>
+#include <queue>
 #include "food.h"
 using namespace std;
-#define VOCAB_SIZE 47
-
+#define VOCAB_SIZE 255
+//47
 struct TrieNode{
     char character;
     Food* food;
@@ -20,7 +21,14 @@ struct TrieNode{
     }
 };
 
+struct PQCompare {
+    bool operator()(const pair<string, float>& p1, const pair<string, float>& p2) const {
+        return p1.second > p2.second;
+    }
+};
+
 class Trie{
+    using PriorityQueue = priority_queue<pair<string, float>, vector<pair<string, float>>, PQCompare>;
     TrieNode* root;
     int item_count = 0;
 
@@ -48,17 +56,7 @@ public:
         string name = food->getKey(); // Fruits
         for (char i : name) {
             char c = tolower(i);
-            int index = c - 'a';
-            if(index == -65 || index == -64) {
-                index += 91; // index 26 and 27
-            }
-            else if(-60 <= index && index <= -56){ // '%'
-                index += 88; // index 28 - 32
-            }
-
-            else if(-52 <= index  && index <= -39){ // '-'
-                index += 85; // index 33 - 46
-            }
+            int index = (int)c;
             if(curr->next[index] == nullptr){
                 curr->next[index] = new TrieNode();
                 curr->next[index]->character = c;
@@ -74,16 +72,7 @@ public:
         TrieNode *curr = root;
         for (char i : word){
             char c = tolower(i);
-            int index = c - 'a';
-            if(index == -65 || index == -64) {
-                index += 91; // index 26 and 27
-            }
-            else if(-60 <= index && index <= -56){ // '%'
-                index += 88; // index 28 - 32
-            }
-            else if(-52 <= index  && index <= -39){ // '-'
-                index += 85; // index 33 - 46
-            }
+            int index = (int)c;
             if(curr->next[index] == nullptr ){
                 return nullptr;
             }
@@ -93,43 +82,41 @@ public:
             return curr->food;
         return nullptr;
     }
-    void iterate(TrieNode* ptr, vector<pair<string, float>> &temp, const string& category){
-        if(ptr == nullptr){
+
+    void findTop10Calories(TrieNode* root, PriorityQueue &PQ, const string& category){
+        if(root == nullptr){
             return;
         }
         for(int i = 0; i < VOCAB_SIZE; i++){
-            iterate(ptr->next[i], temp, category);
-            if(ptr->next[i] != nullptr){
-                if(ptr->next[i]->isWord && category == ptr->next[i]->food->getValue().first){
-                    float norm_cal = ptr->next[i]->food->getValue().second[0];
+            if( root -> next[i] != nullptr){
+                if(root -> next[i] -> isWord && category == root -> next[i] -> food -> getValue().first){
+                    float norm_cal = root -> next[i] -> food -> getValue().second[0];
                     if(norm_cal < 0){
                         continue;
                     }
-                    if (temp.size() < 10) {
-                        temp.emplace_back(ptr->next[i]->food->getKey(), norm_cal);
-                        if (temp.size() == 10) {
-                            sort(temp.begin(), temp.end(), sortbysecondgt);
-                        }
-                    } else {
-                        if (norm_cal > temp.back().second && norm_cal > 0) {
-                            temp.pop_back();
-                            temp.emplace_back(ptr->next[i]->food->getKey(), norm_cal);
-                            sort(temp.begin(), temp.end(), sortbysecondgt);
-                        }
+                    PQ.push(make_pair(root->next[i]->food->getKey(), norm_cal));
+                    if( PQ.size() > 10){
+                        PQ.pop();
                     }
                 }
+                findTop10Calories(root -> next[i], PQ, category);
             }
         }
     }
-    void tenHighestCalorie(const string& category) {
-        vector<pair<string, float>> temp;
-        TrieNode* ptr = root;
-        iterate(ptr, temp, category);
-        sort(temp.begin(), temp.end(), sortbysecondgt);
+
+    void tenHighestCalorie(const string& category){
+        PriorityQueue PQ;
+        findTop10Calories(root, PQ, category);
         cout << "The 10 foods with the highest calorie values are: " << endl;
-        for (int i = 0; i < temp.size(); i++) {
-            cout << i + 1 << ". " << temp[i].first << ": " << temp[i].second << endl;
+        vector<pair<string, float>> top10;
+        while(!PQ.empty()){
+            top10.push_back(PQ.top());
+            PQ.pop();
+        }
+        for (int i = top10.size() - 1, j = 1; i >= 0; i--, j++) {
+            cout << j << ". " << top10[i].first << ": " << top10[i].second << endl;
         }
     }
+
 };
 #endif //TESTING_TRIE_H
