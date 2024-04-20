89fb36697bc1603f1dcd8f2b8b3e543fcf97e6c1
jepin <pinzon.jenrique@ufl.edu>
2024-04-20 18:04:03 -0500

    Updates in trie file - Use of a PQ to display the ten highest and the ten lowest values.

--- src/trie.h
+++ src/trie.h
@@ -21,25 +21,25 @@ struct TrieNode{
     }
 };
 
-struct PQCompare {
+struct MinHeapCompare {
     bool operator()(const pair<string, float>& p1, const pair<string, float>& p2) const {
-        return p1.second > p2.second;
+        return p1.second > p2.second || (p1.second == p2.second && p1.first < p2.first);
+    }
+};
+
+struct MaxHeapCompare {
+    bool operator()(const pair<string, float>& p1, const pair<string, float>& p2) const {
+        return p1.second < p2.second || (p1.second == p2.second && p1.first < p2.first);
     }
 };
 
 class Trie{
-    using PriorityQueue = priority_queue<pair<string, float>, vector<pair<string, float>>, PQCompare>;
+    using PriorityQueueMin = priority_queue<pair<string, float>, vector<pair<string, float>>, MinHeapCompare>;
+    using PriorityQueueMax = priority_queue<pair<string, float>, vector<pair<string, float>>, MaxHeapCompare>;
     TrieNode* root;
     int item_count = 0;
 
-public:
-
-    Trie(){
-        root = new TrieNode();
-    }
-    ~Trie(){
-
-    }
+private:
     void cleanUp(TrieNode* ptr) {
         if (ptr == nullptr) {
             return;
@@ -51,6 +51,73 @@ public:
             }
         }
     }
+
+    int getNutritionIndex(const string& nutrition){
+        //vector<string> nutrition_names={"Calories", "Protein", "Carbohydrates"}
+        int n = -1;
+        if(nutrition == "Calories"){
+            n = 0;
+        } else if(nutrition == "Protein"){
+            n = 1;
+        } else if(nutrition == "Carbohydrates"){
+            n = 2;
+        } else if(nutrition == "Sugars"){
+            n = 3;
+        } else if(nutrition == "Fiber"){
+            n = 4;
+        } else if(nutrition == "Cholesterol"){
+            n = 5;
+        } else if(nutrition == "Saturated Fats"){
+            n = 6;
+        } else if(nutrition == "Trans Fatty Acids"){
+            n = 7;
+        } else if(nutrition == "Soluble Fiber"){
+            n = 8;
+        } else if(nutrition == "Insoluble Fiber"){
+            n = 9;
+        } else if(nutrition == "Monounsaturated Fats"){
+            n = 10;
+        } else if(nutrition == "Polyunsaturated Fats"){
+            n = 11;
+        } else if(nutrition == "Caffeine") {
+            n = 12;
+        }
+        return n;
+    }
+
+    template<typename T, typename C>
+    void findTop10(TrieNode* root, priority_queue<T, vector<T>, C> &PQ,
+                   const string& category, int nutrition_index){
+        if(root == nullptr){
+            return;
+        }
+        for(int i = 0; i < VOCAB_SIZE; i++){
+            if( root -> next[i] != nullptr){
+                if(root -> next[i] -> isWord && category == root -> next[i] -> food -> getValue().first){
+
+                    float val = root -> next[i] -> food -> getValue().second[nutrition_index];
+                    if(val < 0){
+                        continue;
+                    }
+                    PQ.push(make_pair(root->next[i]->food->getKey(), val));
+                    if( PQ.size() > 10){
+                        PQ.pop();
+                    }
+                }
+                findTop10(root -> next[i], PQ, category, nutrition_index);
+            }
+        }
+    }
+
+public:
+
+    Trie(){
+        root = new TrieNode();
+    }
+    ~Trie(){
+
+    }
+
     void insert(Food*& food) {
         TrieNode *curr = root;
         string name = food->getKey(); // Fruits
@@ -112,118 +179,32 @@ public:
         }
     }
 
-    void iterate(TrieNode* ptr, vector<pair<string, float>> &temp, const string& category, const string& nutrition, const string& comp){
-        if(ptr == nullptr){
-            return;
-        }
-        int n;
-        if(nutrition == "Calories"){
-            n = 0;
-        } else if(nutrition == "Protein"){
-            n = 1;
-        } else if(nutrition == "Carbohydrates"){
-            n = 2;
-        } else if(nutrition == "Sugars"){
-            n = 3;
-        } else if(nutrition == "Fiber"){
-            n = 4;
-        } else if(nutrition == "Cholesterol"){
-            n = 5;
-        } else if(nutrition == "Saturated Fats"){
-            n = 6;
-        } else if(nutrition == "Trans Fatty Acids"){
-            n = 7;
-        } else if(nutrition == "Soluble Fiber"){
-            n = 8;
-        } else if(nutrition == "Insoluble Fiber"){
-            n = 9;
-        } else if(nutrition == "Monounsaturated Fats"){
-            n = 10;
-        } else if(nutrition == "Polyunsaturated Fats"){
-            n = 11;
-        } else if(nutrition == "Caffeine") {
-            n = 12;
-        } else{
+    void tenHighest(const string& category, const string& nutrition, const string& comp){
+        int nutrition_index = getNutritionIndex(nutrition);
+        if(nutrition_index < 0 ){
             cout << "Not Valid" << endl;
             return;
         }
-        for(int i = 0; i < VOCAB_SIZE; i++){
-            iterate(ptr->next[i], temp, category, nutrition, comp);
-            if(ptr->next[i] != nullptr){
-                if(ptr->next[i]->isWord && category == ptr->next[i]->food->getValue().first){
-                    float val = ptr->next[i]->food->getValue().second[n];
-                    if(val < 0){
-                        continue;
-                    }
-                    if (temp.size() < 10) {
-                        temp.emplace_back(ptr->next[i]->food->getKey(), val);
-                        if (temp.size() == 10) {
-                            if(comp == "Lowest"){
-                                sort(temp.begin(), temp.end(), sortbysecondlt);
-                            }
-                            else if(comp == "Highest"){
-                                sort(temp.begin(), temp.end(), sortbysecondgt);
-                            }
-                        }
-                    } else {
-                        if (val > temp.back().second && val > 0 && comp == "Highest") {
-                            temp.pop_back();
-                            temp.emplace_back(ptr->next[i]->food->getKey(), val);
-                            sort(temp.begin(), temp.end(), sortbysecondgt);
-                        }else if(val < temp.back().second && val > 0 && comp == "Lowest"){
-                            temp.pop_back();
-                            temp.emplace_back(ptr->next[i]->food->getKey(), val);
-                            sort(temp.begin(), temp.end(), sortbysecondlt);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    void tenHighest(const string& category, const string& nutrition, const string& comp) {
-        vector<pair<string, float>> temp;
-        TrieNode* ptr = root;
-        iterate(ptr, temp, category, nutrition, comp);
-        if(comp == "Lowest"){
-            sort(temp.begin(), temp.end(), sortbysecondlt);
-        }
-        else{
-            sort(temp.begin(), temp.end(), sortbysecondgt);
-        }
-        cout << "The 10 foods with the highest calorie values are: " << endl;
-        for (int i = 0; i < temp.size(); i++) {
-            cout << i + 1 << ". " << temp[i].first << ": " << temp[i].second << endl;
-        }
-    }
-    void findTop10Calories(TrieNode* root, PriorityQueue &PQ, const string& category){
-        if(root == nullptr){
-            return;
-        }
-        for(int i = 0; i < VOCAB_SIZE; i++){
-            if( root -> next[i] != nullptr){
-                if(root -> next[i] -> isWord && category == root -> next[i] -> food -> getValue().first){
-                    float norm_cal = root -> next[i] -> food -> getValue().second[0];
-                    if(norm_cal < 0){
-                        continue;
-                    }
-                    PQ.push(make_pair(root->next[i]->food->getKey(), norm_cal));
-                    if( PQ.size() > 10){
-                        PQ.pop();
-                    }
-                }
-                findTop10Calories(root -> next[i], PQ, category);
-            }
-        }
-    }
-    void tenHighestCalorie(const string& category){
-        PriorityQueue PQ;
-        findTop10Calories(root, PQ, category);
-        cout << "The 10 foods with the highest calorie values are: " << endl;
         vector<pair<string, float>> top10;
-        while(!PQ.empty()){
-            top10.push_back(PQ.top());
-            PQ.pop();
+        string label = "lowest";
+        if(comp == "Lowest"){
+            PriorityQueueMax PQ;
+            findTop10(root, PQ, category, nutrition_index);
+            while(!PQ.empty()){
+                top10.push_back(PQ.top());
+                PQ.pop();
+            }
+        }else{
+            label = "highest";
+            PriorityQueueMin PQ;
+            findTop10(root, PQ, category, nutrition_index);
+            while(!PQ.empty()){
+                top10.push_back(PQ.top());
+                PQ.pop();
+            }
         }
+
+        cout << "The 10 foods with the "<<label<<" values are: " << endl;
         for (int i = top10.size() - 1, j = 1; i >= 0; i--, j++) {
             cout << j << ". " << top10[i].first << ": " << top10[i].second << endl;
         }
