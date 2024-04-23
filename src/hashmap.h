65afa6a4ab21f9ccf9e824744a4b94e4281c643e
dyl <anduytran@ufl.edu>
2024-04-22 21:44:30 -0400

    Added comments

--- src/hashmap.h
+++ src/hashmap.h
@@ -18,28 +18,22 @@ class HashMap{
     int max_size;
     float lf = .56;
 
-    //  Create an alias for a MinHeap and MaxHeap (PQ)
     using PriorityQueueMin = priority_queue<pair<string, float>, vector<pair<string, float>>, MinHeapCompare>;
     using PriorityQueueMax = priority_queue<pair<string, float>, vector<pair<string, float>>, MaxHeapCompare>;
 
 private:
     template<typename T, typename C>
-    // Generic Function to get the ten lowest or ten highest values using a MaxHeap or MinHeap
     void findTop10(priority_queue<T, vector<T>, C> &PQ,
                    const string& category, int nutrition_index){
-        // Iterate over hashmap
+
         for (int i = 0; i < getSize(); i++) {
             if (map[i] != nullptr) {
-                // If a valid category was found
                 if (map[i]->getValue().first == category || category == "All") {
-                    // Get the value, given the nutrition index
                     float val = map[i]->getValue().second[nutrition_index];
                     if(val < 0){
                         continue;
                     }
-                    // Store the name and value of the current food in the PQ
                     PQ.push(make_pair(map[i]->getKey(), val));
-                    // If the size is larger than ten, we pop that element from the PQ
                     if( PQ.size() > 10){
                         PQ.pop();
                     }
@@ -49,35 +43,35 @@ private:
     }
 
 public:
-    hash<string> hash;
+    hash<string> hash; // using the hash function from STL
     HashMap(){
-        max_size = 400;
+        max_size = 400; // because we know how big our map is, we can start "bigger" just to reduce amount of rehashings
         vector<Food*> v(max_size);
         this->map = v;
     }
     ~HashMap(){
-        for(auto & i : map) {
-            if (i != nullptr) {
+        for(auto & i : map) { // iterate through map
+            if (i != nullptr) { // if the item in map is actually a food item and not nullptr
                 Food *ptr = i;
                 delete ptr;
-                i = nullptr;
+                i = nullptr; // delete and reassign to nullptr
             }
         }
     }
     int getSize() const{
-        return max_size;
+        return max_size; // get the max capacity
     }
     int getCurrentSize() const{
-        return current_size;
+        return current_size; // get the current size
     }
-    void search(const string& s){
+    void search(const string& s){ // look for item in map with only key-words
         int entries = 0;
         string output = "";
         for(int i = 0; i < map.size(); i++){
             if(map[i] != nullptr){
                 string str = map[i]->getKey();
                 auto it = ::search(str.begin(), str.end(), s.begin(), s.end(),
-                                   caseinsensitive);
+                                   caseinsensitive); // use custom comparator for case insensitivity
                 if(it != str.end()){
                     entries++;
                     output += map[i]->getKey() + "\n";
@@ -86,21 +80,21 @@ public:
         }
         cout << entries << " entries found for \"" << s << "\"" << endl << output << endl;
     }
-    void set(string name, Food* food){
-        current_size += 1;
+    void set(string name, Food* food){ // set value in hashmap
+        current_size += 1; // increase size
         int index;
-        if((float)current_size / (float)max_size >= lf) {
+        if((float)current_size / (float)max_size >= lf) { // if resize and rehash,
             max_size *= 2;
-            vector<Food *> temp(max_size);
-            for (auto &i: map) {
-                if (i != nullptr) {
+            vector<Food *> temp(max_size); // create a new vector that is double the previous size
+            for (auto &i: map) { // iterate
+                if (i != nullptr) { // if there is an item in the previous vector, rehash
                     index = hash(i->getKey()) % (max_size - 5);
-                    if (temp[index] == nullptr) {
+                    if (temp[index] == nullptr) { // if there is no collision
                         temp[index] = i;
-                    } else if (temp[index]->getKey() != i->getKey()) {
+                    } else if (temp[index]->getKey() != i->getKey()) { // if there is a collision
                         for(int j = 0; j < max_size; j++){
-                            index = (hash(i->getKey()) + (j * j)) % (max_size - 5);
-                            if(!temp[index]){
+                            index = (hash(i->getKey()) + (j * j)) % (max_size - 5); // quadratic probing
+                            if(!temp[index]){ // until you find a spot that's not taken
                                 temp[index] = i;
                                 break;
                             }
@@ -108,15 +102,15 @@ public:
                     }
                 }
             }
-            map = temp;
+            map = temp; // reassign the map ptr to the new vector
         }
-        index = hash(name) % (max_size - 5);
-        if(map[index] == nullptr){
+        index = hash(name) % (max_size - 5); // recalculate index after rehashing or for the first time
+        if(map[index] == nullptr){ // if no collision
             map[index] = food;
         }
-        else if(map[index]->getKey() != name){
+        else if(map[index]->getKey() != name){ // if there is a collision, and this item is not the owner of the spot
             for(int j = 0; j < max_size; j++){
-                index = (hash(name) + (j * j)) % (max_size - 5);
+                index = (hash(name) + (j * j)) % (max_size - 5); // quadratic probing
                 if(!map[index]){
                     map[index] = food;
                     break;
@@ -124,16 +118,16 @@ public:
             }
         }
     }
-    Food* get(string name){
-        int index = hash(name) % (max_size - 5);
-        if(map[index] != nullptr) {
-            if (map[index]->getKey() == name) {
+    Food* get(string name){ // retrieve item from hashmap
+        int index = hash(name) % (max_size - 5); // calculate index
+        if(map[index] != nullptr) { // if the hash calculates hits a spot that's not empty
+            if (map[index]->getKey() == name) { // if this spot is the "name's" spot,
                 return map[index];
-            } else if (map[index]->getKey() != name) {
+            } else if (map[index]->getKey() != name) { // if this spot is not the "name's" spot
                 for(int j = 0; j < max_size; j++){
-                    index = (hash(name) + (j * j)) % (max_size - 5);
+                    index = (hash(name) + (j * j)) % (max_size - 5); // quadratic probing
                     if(map[index] != nullptr) {
-                        if (map[index]->getKey() == name) {
+                        if (map[index]->getKey() == name) { // if quadratic probing finds a new item, and it is the correct item
                             return map[index];
                         }
                     }
@@ -143,42 +137,32 @@ public:
             return nullptr;
         }
     }
-    // Function to print the top ten food with the highest or lowest nutritional values
     void tenValues(const string& category, const string& nutrition, const string& comp){
-        int nutrition_index = getNutritionIndex(nutrition);
-        // Checking if nutrition index is valid
-        if(nutrition_index < 0 ){
+        int nutrition_index = getNutritionIndex(nutrition); // get correct index for nutrition
+        if(nutrition_index < 0 ){ // getNutritionIndex() returns -1 for incorrect strings
             cout << "Not Valid" << endl;
             return;
         }
-        // Decide whether to display the lowest or highest values
         vector<pair<string, float>> top10;
         string label = "lowest";
-        if(comp == "Lowest"){
-            // Create a MaxHeap
+        if(comp == "Lowest"){ // min heap
             PriorityQueueMax PQ;
-            // Find top ten food names and values
             findTop10(PQ, category, nutrition_index);
-            // Pop the elements from the MaxHeap and store them in the top ten vector
             while(!PQ.empty()){
                 top10.push_back(PQ.top());
                 PQ.pop();
             }
-        }else{
+        }else{ // max heap
             label = "highest";
-            // Create a MinHeap
             PriorityQueueMin PQ;
-            // Find top ten food names and values
             findTop10(PQ, category, nutrition_index);
-            // Pop the elements from the MinHeap and store them in the top ten vector
             while(!PQ.empty()){
                 top10.push_back(PQ.top());
                 PQ.pop();
             }
         }
-        // Print out the results
         cout << "Using the Hashmap, " << endl;
-        cout << "The 10 foods with the "<< label << " " << nutrition << " are: " << endl;
+        cout << "The 10 foods [100 (g) serving sizes] with the "<< label << " " << nutrition << " are: " << endl;
         for (int i = top10.size() - 1, j = 1; i >= 0; i--, j++) {
             cout << j << ". " << top10[i].first << ": " << top10[i].second << endl;
         }
